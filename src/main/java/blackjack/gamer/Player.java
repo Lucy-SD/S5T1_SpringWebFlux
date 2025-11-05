d1eecb99394809297f5585e7225e5ed6be9a36c4
Lucy C. <you@example.com>
2025-11-06 00:03:13 +0100

    feat: implement scoreCalculator to Player

--- src/main/java/blackjack/gamer/Player.java
+++ src/main/java/blackjack/gamer/Player.java
@@ -1,6 +1,7 @@
 package blackjack.gamer;
 
 import blackjack.deck.Card;
+import blackjack.game.score.ScoreCalculator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +11,13 @@ public class Player implements Gamer {
     private String name;
     private List<Card> hand;
     private int score;
+    private ScoreCalculator scoreCalculator;
 
     public Player(String name) {
         this.name = name;
         this.hand = new ArrayList<>();
         this.score = 0;
+        this.scoreCalculator = new ScoreCalculator();
     }
 
     @Override
@@ -34,5 +37,6 @@ public class Player implements Gamer {
 
     public void addCard(Card card) {
         this.hand.add(card);
+        this.score = scoreCalculator.calculate(this.hand);
     }
 }
