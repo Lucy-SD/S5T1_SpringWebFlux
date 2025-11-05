65c3a87fdb5650aaf5749d2118cdf79647c037f7
Lucy C. <you@example.com>
2025-11-05 18:32:35 +0100

    shuffleDeck_shouldReturnMixedDeck test

--- src/test/java/blackjack/DeckServiceTest.java
+++ src/test/java/blackjack/DeckServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,13 +20,23 @@ class DeckServiceTest {
 
     @Test
     void createDeck_shouldGenerateA52CardsDeck() {
-
         List<Card> deck = deckService.createDeck();
 
         assertThat(deck).hasSize(52);
     }
 
+    @Test
+    void shuffleDeck_shouldReturnMixedDeck() {
+        List<Card> deck = deckService.createDeck();
 
+        List<Card> deck1 = new ArrayList<>(deck);
+        List<Card> deck2 = new ArrayList<>(deck);
+
+        List<Card> shuffled1 = deckService.shuffleDeck(deck1);
+        List<Card> shuffled2 = deckService.shuffleDeck(deck2);
+
+        assertThat(shuffled1).isNotEqualTo(shuffled2);
+    }
 
 
 
