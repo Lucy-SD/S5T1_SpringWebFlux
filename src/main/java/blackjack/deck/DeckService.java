4dda1b87b9104ab039ef2d93b2de7ef698815d4a
Lucy C. <you@example.com>
2025-11-05 18:14:44 +0100

    refact: createDeck only creates deck

--- src/main/java/blackjack/deck/DeckService.java
+++ src/main/java/blackjack/deck/DeckService.java
@@ -1,9 +1,8 @@
 package blackjack.deck;
 
-import reactor.core.publisher.Flux;
-
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class DeckService {
 
@@ -13,13 +12,11 @@ public class DeckService {
         deck = new ArrayList<>();
     }
 
-    public Flux<Card> createDeck() {
+    public List<Card> createDeck() {
         for (int value = 1; value <= 9; value++) {
             deck.addAll(Collections.nCopies(4, new Card(value)));
         }
         deck.addAll(Collections.nCopies(16, new Card(10)));
-
-
-        return Flux.fromIterable(deck);
+        return deck;
     }
 }
--- src/test/java/blackjack/DeckServiceTest.java
+++ src/test/java/blackjack/DeckServiceTest.java
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 class DeckServiceTest {
@@ -18,12 +19,11 @@ class DeckServiceTest {
     @Test
     void createDeck_shouldGenerateA52CardsDeck() {
 
-        Flux<Card> deck = deckService.createDeck();
+        List<Card> deck = deckService.createDeck();
 
-        StepVerifier.create(deck)
-                .expectNextCount(52)
-                .verifyComplete();
+        assertThat(deck).hasSize(52);
     }
+
 
 
 
