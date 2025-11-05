acba85db13c7e7a539f16557a8f0ac75c8d02e45
Lucy C. <you@example.com>
2025-11-05 23:34:03 +0100

    feat: add playerAddsCard_shouldAddCardToHand test

--- src/test/java/blackjack/PlayerTest.java
+++ src/test/java/blackjack/PlayerTest.java
@@ -1,5 +1,6 @@
 package blackjack;
 
+import blackjack.deck.Card;
 import blackjack.gamer.Gamer;
 import blackjack.gamer.Player;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,14 @@ class PlayerTest {
         Gamer player = new Player("Pepe");
         assertThat(player.getHand()).isEmpty();
         assertThat(player.getScore()).isZero();
+    }
+
+    @Test
+    void playerAddsCard_shouldAddCardToHand() {
+        Gamer player = new Player("Pepe");
+        player.addCard(new Card(7));
+        assertThat(player.getHand()).hasSize(1);
+        assertThat(player.getHand().get(0).value()).isEqualTo(7);
     }
 
 }
