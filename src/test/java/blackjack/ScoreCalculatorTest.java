3f4378530e4b49ca2c2b1c8ac22fb97a9854651c
Lucy C. <you@example.com>
2025-11-06 00:30:38 +0100

    feat: add tests to analyse Ace behaviour

--- src/test/java/blackjack/ScoreCalculatorTest.java
+++ src/test/java/blackjack/ScoreCalculatorTest.java
@@ -16,4 +16,25 @@ class ScoreCalculatorTest {
         ScoreCalculator calculator = new ScoreCalculator();
         assertThat(calculator.calculate(hand)).isEqualTo(15);
     }
+
+    @Test
+    void calculateScore_withAceAndScoreUnder21_shouldValueAceAs11() {
+        List<Card> hand = List.of(new Card(7), new Card(1));
+        ScoreCalculator calculator = new ScoreCalculator();
+        assertThat(calculator.calculate(hand)).isEqualTo(18);
+    }
+
+    @Test
+    void calculateScore_withAceAndScoreOver21_shouldValueAceAs1() {
+        List<Card> hand = List.of(new Card(7), new Card(1), new Card(8), new Card(5));
+        ScoreCalculator calculator = new ScoreCalculator();
+        assertThat(calculator.calculate(hand)).isEqualTo(21);
+    }
+
+    @Test
+    void calculateScore_withMoreThanOneAce_shouldValueOneAceAs11AndOthersAs1() {
+        List<Card> hand = List.of(new Card(1), new Card(1), new Card(7));
+        ScoreCalculator calculator = new ScoreCalculator();
+        assertThat(calculator.calculate(hand)).isEqualTo(19);
+    }
 }
