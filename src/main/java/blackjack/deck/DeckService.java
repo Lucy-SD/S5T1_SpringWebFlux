e8bd5f6dfd73a16cfd9b3b6b6e60c08b20d4831c
Lucy C. <you@example.com>
2025-11-05 22:18:25 +0100

    changing DeckService to return a shuffled deck

--- src/main/java/blackjack/deck/DeckService.java
+++ src/main/java/blackjack/deck/DeckService.java
@@ -6,17 +6,20 @@ import java.util.List;
 
 public class DeckService {
 
-    private ArrayList<Card> deck;
-
-    public DeckService() {
-        deck = new ArrayList<>();
-    }
-
-    public List<Card> createDeck() {
+    private List<Card> createDeck() {
+        List<Card> deck = new ArrayList<>();
         for (int value = 1; value <= 9; value++) {
             deck.addAll(Collections.nCopies(4, new Card(value)));
         }
         deck.addAll(Collections.nCopies(16, new Card(10)));
         return deck;
     }
+
+    private List<Card> shuffleDeck(List<Card> deck) {
+        for (int i = 0; i <= 7; i++) {
+            Collections.shuffle(deck);
+        }
+        return deck;
+    }
+
 }
--- src/test/java/blackjack/DeckServiceTest.java
+++ src/test/java/blackjack/DeckServiceTest.java
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,25 +20,18 @@ class DeckServiceTest {
     private DeckService deckService;
 
     @Test
-    void createDeck_shouldGenerateA52CardsDeck() {
-        List<Card> deck = deckService.createDeck();
+    void createShuffledDeck_returnsFluxOf52ShuffledCards() {
+        Flux<Card> deck = deckService.createShuffledDeck();
+        Flux<Card> deck2 = deckService.createShuffledDeck();
 
-        assertThat(deck).hasSize(52);
-    }
+        StepVerifier.create(deck)
+                .expectNextCount(52)
+                .verifyComplete();
 
-    @Test
-    void shuffleDeck_shouldReturnMixedDeck() {
-        List<Card> deck = deckService.createDeck();
-
-        List<Card> deck1 = new ArrayList<>(deck);
-        List<Card> deck2 = new ArrayList<>(deck);
-
-        List<Card> shuffled1 = deckService.shuffleDeck(deck1);
-        List<Card> shuffled2 = deckService.shuffleDeck(deck2);
+        List<Card> shuffled1 = deck.collectList().block();
+        List<Card> shuffled2 = deck2.collectList().block();
 
         assertThat(shuffled1).isNotEqualTo(shuffled2);
     }
-
-
 
 }
