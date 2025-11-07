65c2387fbcb293f93ba76d089438c9487435b2b2
Lucy C. <you@example.com>
2025-11-07 15:25:52 +0100

    add card mocker to test

--- src/test/java/blackjack/game/GameServiceTest.java
+++ src/test/java/blackjack/game/GameServiceTest.java
@@ -1,15 +1,21 @@
 package blackjack.game;
 
+import blackjack.deck.Card;
 import blackjack.deck.DeckService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class GameServiceTest {
@@ -22,6 +28,15 @@ class GameServiceTest {
 
     @Test
     void startNewGame_setsCorrectGameState() {
+        List<Card> mockCards = Arrays.asList(
+                new Card(10),
+                new Card(7),
+                new Card(8),
+                new Card(10)
+        );
+
+        when(deckService.createShuffledDeck()).thenReturn(Flux.fromIterable(mockCards));
+
         Mono<GameState> gameStateMono = gameService.startNewGame("Pepe");
         GameState gameState = gameStateMono.block();
 
