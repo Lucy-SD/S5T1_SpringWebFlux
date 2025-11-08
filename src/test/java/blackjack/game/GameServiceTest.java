a9d85b684c7aad9aff0f410abd228a8a5c8bd919
Lucy C. <you@example.com>
2025-11-08 10:38:11 +0100

    feat: add playerCanHit&updateState

--- src/test/java/blackjack/game/GameServiceTest.java
+++ src/test/java/blackjack/game/GameServiceTest.java
@@ -19,6 +19,7 @@ import reactor.core.publisher.Mono;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -55,18 +56,46 @@ class GameServiceTest {
     }
 
     @Test
-    void playerCannotHitWhenScores21OrMore() {
+    void playerCannotHit_whenScoreIs21OrMore() {
         List<Card> mockCards = Arrays.asList(
                 new Card(1),
                 new Card(2),
                 new Card(10),
                 new Card(10)
-                );
+        );
         when(deckService.createShuffledDeck()).thenReturn(Flux.fromIterable(mockCards));
         GameState gameState = gameService.startNewGame("Pepe").block();
 
         assertThatExceptionOfType(GameException.class)
                 .isThrownBy(() -> gameService.playerHit(gameState).block())
                 .withMessageContaining("no puede pedir cartas");
+    }
+
+    @Test
+    void playerCanHit_whenScoreIsUnder21_thenCardIsAddedToHandAndScoreIsUpdated() {
+        List<Card> mockCards = Arrays.asList(
+                new Card(5),
+                new Card(7),
+                new Card(5),
+                new Card(5),
+                new Card(3)
+        );
+
+        when(deckService.createShuffledDeck()).thenReturn(Flux.fromIterable(mockCards));
+        GameState gameState = gameService.startNewGame("Pepe").block();
+
+        assertThat(gameState.getPlayer().getHand()).hasSize(2);
+        assertThat(gameState.getPlayer().getScore()).isEqualTo(10);
+
+        GameState newState = gameService.playerHit(gameState).block();
+
+        assertThat(newState.getPlayer().getHand()).hasSize(3);
+        assertThat(newState.getPlayer().getScore()).isEqualTo(13);
+
+        List<Integer> cardValues = newState.getPlayer().getHand()
+                .stream()
+                .map(Card::value)
+                .collect(Collectors.toList());
+        assertThat(cardValues).containsExactly(5, 5, 3);
     }
 }
