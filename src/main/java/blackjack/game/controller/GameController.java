69939dac4eccde72fd56d6dba6952e5b3fcef059
Lucy C. <you@example.com>
2025-11-10 01:16:12 +0100

    feat: add playGame endpoint

--- src/main/java/blackjack/game/controller/GameController.java
+++ src/main/java/blackjack/game/controller/GameController.java
@@ -2,10 +2,17 @@ package blackjack.game.controller;
 
 import blackjack.exception.GameException;
 import blackjack.game.dto.request.CreateGameRequest;
+import blackjack.game.dto.request.PlayRequest;
 import blackjack.game.dto.response.GameResponse;
+import blackjack.game.dto.response.PlayResponse;
 import blackjack.game.mapper.GameMapper;
 import blackjack.game.mapper.GameResponseMapper;
+import blackjack.game.model.GameResult;
+import blackjack.game.model.GameState;
+import blackjack.game.model.GameStatus;
+import blackjack.game.model.PlayerAction;
 import blackjack.game.service.GameManagerService;
+import blackjack.game.service.PlayGameService;
 import blackjack.game.service.ReadGameService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,20 +26,27 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class GameController {
     private final ReadGameService readGame;
+    private final PlayGameService playGame;
     private final GameManagerService gameManager;
     private final GameMapper mapper;
     private final GameResponseMapper responseMapper;
+
+    private record Tuple(GameState gameState, GameResult gameResult, GameStatus status) {
+        static Tuple of(GameState gameState, GameResult gameResult, GameStatus status) {
+            return new Tuple(gameState, gameResult, status);
+        }
+    }
 
     @PostMapping("/new")
     public Mono<ResponseEntity<GameResponse>> createGame(@Valid @RequestBody CreateGameRequest request) {
         return gameManager.createNewGame(request.playerName())
                 .flatMap(gameEntity ->
                         mapper.toGameState(gameEntity)
-                        .map(gameState -> responseMapper.toGameResponse(
-                                gameState,
-                                gameEntity.getId(),
-                                gameEntity.getStatus()
-                        ))
+                                .map(gameState -> responseMapper.toGameResponse(
+                                        gameState,
+                                        gameEntity.getId(),
+                                        gameEntity.getStatus()
+                                ))
                 )
                 .map(gameResponse ->
                         ResponseEntity.status(HttpStatus.CREATED).body(gameResponse)
@@ -53,6 +67,63 @@ public class GameController {
                 )
                 .onErrorResume(GameException.class, e ->
                         Mono.just(ResponseEntity.notFound().build())
+                );
+    }
+
+    @PostMapping("/{gameId}/play")
+    public Mono<ResponseEntity<PlayResponse>> playGame(
+            @PathVariable String gameId,
+            @Valid @RequestBody PlayRequest request) {
+        return readGame.findGameById(gameId)
+                .switchIfEmpty(Mono.error(new GameException("No se encontró la partida.")))
+                .flatMap(gameEntity -> {
+                    if (gameEntity.getStatus() != GameStatus.ACTIVE) {
+                        return Mono.error(new GameException("Partida finalizada."));
+                    }
+                    return mapper.toGameState(gameEntity);
+                })
+                .flatMap(gameState -> {
+                    if (gameState.getPlayer().getScore() >= 21 && request.action() == PlayerAction.HIT) {
+                        return Mono.error(new GameException("El jugador ya no puede pedir cartas."));
+                    }
+                    switch (request.action()) {
+                        case HIT -> {
+                            return gameManager.playerHit(gameId);
+                        }
+                        case STAND -> {
+                            return gameManager.playerStand(gameId);
+                        }
+                        default -> {
+                            return Mono.error(new GameException("Acción no válida."));
+                        }
+                    }
+                })
+                .flatMap(gameEntity ->
+                        mapper.toGameState(gameEntity)
+                                .flatMap(gameState ->
+                                        playGame.findOutWinner(gameState)
+                                                .flatMap(gameResult -> {
+                                                    boolean gameFinished = gameResult.winner() != null;
+                                                    GameStatus status = gameFinished ? GameStatus.FINISHED : GameStatus.ACTIVE;
+                                                    if (gameFinished) {
+                                                        return gameManager.finishGame(gameEntity.getId())
+                                                                .then(Mono.just(Tuple.of(gameState, gameResult, status)));
+                                                    } else {
+                                                        return Mono.just(Tuple.of(gameState, gameResult, status));
+                                                    }
+                                                })
+                                )
+                )
+                .map(tuple -> {
+                    GameResponse gameResponse = responseMapper.toGameResponse(
+                            tuple.gameState(), gameId, tuple.status()
+                    );
+                    return new PlayResponse(tuple.status(), gameResponse, tuple.status() == GameStatus.FINISHED ?
+                            tuple.gameResult() : null);
+                })
+                .map(ResponseEntity::ok)
+                .onErrorResume(GameException.class, e ->
+                        Mono.just(ResponseEntity.badRequest().build())
                 );
     }
 }
