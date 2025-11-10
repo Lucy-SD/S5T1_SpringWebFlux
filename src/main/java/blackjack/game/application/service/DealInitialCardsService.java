49924e45d5d6674c8abbc0d88deb3e3537ba40b7
Lucy C. <you@example.com>
2025-11-11 00:36:56 +0100

    add DealIniialCardsService

---
+++ src/main/java/blackjack/game/application/service/DealInitialCardsService.java
@@ -0,0 +1,40 @@
+package blackjack.game.application.service;
+
+import blackjack.deck.application.service.CardDrawer;
+import blackjack.game.application.usecase.DealInitialCards;
+import blackjack.game.domain.Game;
+import blackjack.shared.ScoreCalculator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class DealInitialCardsService implements DealInitialCards {
+    private final CardDrawer cardDrawer;
+    private final ScoreCalculator scoreCalculator;
+
+    @Override
+    public Mono<Game> deal(Game game) {
+        return cardDrawer.drawCard()
+                .flatMap(card1 -> {
+                    game.getPlayerHand().add(card1);
+                    return cardDrawer.drawCard();
+                })
+                .flatMap(card2 -> {
+                    game.getDealerHand().add(card2);
+                    game.setDealerScore(card2.value());
+                    return cardDrawer.drawCard();
+                })
+                .flatMap(card3 -> {
+                    game.getPlayerHand().add(card3);
+                    return cardDrawer.drawCard();
+                })
+                .map(card4 -> {
+                    game.getDealerHand().add(card4);
+
+                    game.setPlayerScore(scoreCalculator.calculate(game.getPlayerHand()));
+                    return game;
+                });
+    }
+}
